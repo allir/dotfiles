bbebc2be5d5bafc8d61415a764f37e666554cb82
Aðalsteinn Rúnarsson <alli@allir.org>
2022-10-07 14:44:30 +0000

    Add default SSH config file and include dir

---
+++ ssh-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This is pretty ugly, but it works on macOS without requiring coreutils for GNU readlink
+# Ideally this would be:
+# SCRIPT=$( readlink -f ${BASH_SOURCE[0]} )
+SCRIPT=$( cd "$(dirname ${BASH_SOURCE[0]})" &>/dev/null && pwd )/$( basename ${BASH_SOURCE[0]} )
+SCRIPTPATH=$( dirname ${SCRIPT} )
+
+# Make sure ~/.ssh/ exists and create ~/.ssh/config.d 
+umask 077
+mkdir -p ~/.ssh/config.d
+
+ln -snf ${SCRIPTPATH}/.ssh/config ~/.ssh
