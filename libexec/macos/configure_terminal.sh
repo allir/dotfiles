f386e8cf44d8db85421ec92d60cd8a28cc6862c6
Aðalsteinn Rúnarsson <alli@allir.org>
2025-03-25 10:41:51 +0000

    Fix Brewfile and Add macOS Terminal configuration

--- libexec/macos/configure_terminal.sh
+++ libexec/macos/configure_terminal.sh
@@ -9,9 +9,15 @@ SCRIPTPATH=$( dirname "${SCRIPT}" )
 PROFILEPATH=$( realpath "${SCRIPTPATH}/../../var/lib/terminal/allir.terminal" )
 
 #osascript "${SCRIPTPATH}/configure_terminal.scpt" "${PROFILEPATH}"
+echo "Importing Terminal profile, this will open a new Terminal window"
+sleep 1
 open "${PROFILEPATH}"
 
+echo "Setting the default profile in Terminal"
 defaults write com.apple.Terminal "Default Window Settings" -string "allir"
 defaults write com.apple.Terminal "Startup Window Settings" -string "allir"
 
+echo "This will close all Terminal windows afterwards, to cancel press Ctrl+C"
+echo "Continuing in 5 seconds..."
+sleep 5
 killall Terminal
